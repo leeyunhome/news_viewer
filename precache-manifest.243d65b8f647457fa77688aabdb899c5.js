9f7a61475e38a8955b58582f803c14f3e2241dff
leeyunhome <leeyunhome@daum.net>
2020-04-22 14:56:57 +0900

    Updates

---
+++ precache-manifest.243d65b8f647457fa77688aabdb899c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae3874576d748f6ac18836ddf26a46d",
+    "url": "/news_viewer/index.html"
+  },
+  {
+    "revision": "c8e3e010f19d6da13898",
+    "url": "/news_viewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "235182dbe60dff0d6e0c",
+    "url": "/news_viewer/static/js/2.599a93f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news_viewer/static/js/2.599a93f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e3e010f19d6da13898",
+    "url": "/news_viewer/static/js/main.a23cb35f.chunk.js"
+  },
+  {
+    "revision": "a050beef41cd324515f8",
+    "url": "/news_viewer/static/js/runtime-main.317c6353.js"
+  }
+]);
